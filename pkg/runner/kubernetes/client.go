d11657d5813a96e4b6248954edfd530be6366e9c
rick <1450685+LinuxSuRen@users.noreply.github.com>
2023-04-26 03:15:59 +0800

    feat: support to verify specific k8s resources count

--- pkg/runner/kubernetes/client.go
+++ pkg/runner/kubernetes/client.go
@@ -86,6 +86,7 @@ type defaultResourceValidator struct {
 	err  error
 }
 
+// Exist returns true if the specific resource exist
 func (v *defaultResourceValidator) Exist() bool {
 	if v.err != nil {
 		fmt.Println(v.err)
@@ -94,6 +95,7 @@ func (v *defaultResourceValidator) Exist() bool {
 	return v.data != nil && len(v.data) > 0
 }
 
+// Count returns the count of the specific resources
 func (v *defaultResourceValidator) Count() int {
 	val, ok, err := unstructured.NestedField(v.data, "items")
 	if ok && err == nil {
@@ -103,10 +105,12 @@ func (v *defaultResourceValidator) Count() int {
 	return -1
 }
 
+// ExpectCount returns true if there are specific number of the target k8s resources
 func (v *defaultResourceValidator) ExpectCount(expect int) bool {
 	return expect == v.Count()
 }
 
+// ExpectField verify the specific field and value
 func (v *defaultResourceValidator) ExpectField(value interface{}, fields ...string) (result bool) {
 	val, ok, err := unstructured.NestedField(v.data, fields...)
 	if !ok || err != nil {
