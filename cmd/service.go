98ded801ea3af7a965f7fe0b5d88c6baffe48834
rick <1450685+LinuxSuRen@users.noreply.github.com>
2023-07-21 22:50:05 +0800

    feat: support start atest as container service

--- cmd/service.go
+++ cmd/service.go
@@ -7,6 +7,7 @@ import (
 
 	_ "embed"
 
+	"github.com/linuxsuren/api-testing/pkg/version"
 	fakeruntime "github.com/linuxsuren/go-fake-runtime"
 	"github.com/spf13/cobra"
 )
@@ -18,13 +19,21 @@ func createServiceCommand(execer fakeruntime.Execer) (c *cobra.Command) {
 	c = &cobra.Command{
 		Use:     "service",
 		Aliases: []string{"s"},
-		Short:   "Install atest as a Linux service",
+		Example: `atest service install
+atest service start`,
+		Short:   "Install atest as service",
+		Long:    `It could be a native or container service`,
 		PreRunE: opt.preRunE,
 		RunE:    opt.runE,
 	}
 	flags := c.Flags()
-	flags.StringVarP(&opt.action, "action", "a", "", "The action of service, support actions: install, start, stop, restart, status")
+	flags.StringVarP(&opt.action, "action", "a", "",
+		fmt.Sprintf("The action of the service, available values: %v", Action("").All()))
 	flags.StringVarP(&opt.scriptPath, "script-path", "", "", "The service script file path")
+	flags.StringVarP(&opt.mode, "mode", "m", string(ServiceModeOS),
+		fmt.Sprintf("Availeble values: %v", ServiceModeOS.All()))
+	flags.StringVarP(&opt.image, "image", "", defaultImage, "The image of the service which as a container")
+	flags.StringVarP(&opt.version, "version", "", version.GetVersion(), "The version of the service image")
 	return
 }
 
@@ -32,33 +41,59 @@ type serviceOption struct {
 	action     string
 	scriptPath string
 	service    Service
+	image      string
+	version    string
 	fakeruntime.Execer
+	mode string
+}
+
+type serviceMode string
+
+const (
+	ServiceModeOS        serviceMode = "os"
+	ServiceModeContainer serviceMode = "container"
+	ServiceModePodman    serviceMode = "podman"
+	ServiceModeDocker    serviceMode = "docker"
+)
+
+func (s serviceMode) All() []serviceMode {
+	return []serviceMode{ServiceModeOS, ServiceModeContainer,
+		ServiceModePodman, ServiceModeDocker}
 }
 
 func (o *serviceOption) preRunE(c *cobra.Command, args []string) (err error) {
-	if o.Execer.OS() != fakeruntime.OSLinux && o.Execer.OS() != fakeruntime.OSDarwin {
-		err = fmt.Errorf("only support on Linux/Darwin instead of %s", o.Execer.OS())
-	} else {
-		if o.action == "" && len(args) > 0 {
-			o.action = args[0]
-		}
-		o.service = newService(o.Execer, o.scriptPath)
+	if o.action == "" && len(args) > 0 {
+		o.action = args[0]
+	}
+
+	switch serviceMode(o.mode) {
+	case ServiceModeOS:
+		o.service, err = o.getOSService()
+	default:
+		o.service, err = o.getContainerService()
+	}
+
+	if o.service == nil {
+		err = fmt.Errorf("not supported service")
+		return
 	}
 	return
 }
 
 func (o *serviceOption) runE(c *cobra.Command, args []string) (err error) {
 	var output string
-	switch o.action {
-	case "install", "i":
+	switch Action(o.action) {
+	case ActionInstall:
 		output, err = o.service.Install()
-	case "start":
+	case ActionUninstall:
+		output, err = o.service.Uninstall()
+	case ActionStart:
 		output, err = o.service.Start()
-	case "stop":
+	case ActionStop:
 		output, err = o.service.Stop()
-	case "restart":
+	case ActionRestart:
 		output, err = o.service.Restart()
-	case "status":
+	case ActionStatus:
 		output, err = o.service.Status()
 	default:
 		err = fmt.Errorf("not support action: '%s'", o.action)
@@ -70,6 +105,23 @@ func (o *serviceOption) runE(c *cobra.Command, args []string) (err error) {
 	return
 }
 
+type Action string
+
+const (
+	ActionInstall   Action = "install"
+	ActionUninstall Action = "uninstall"
+	ActionStart     Action = "start"
+	ActionStop      Action = "stop"
+	ActionRestart   Action = "restart"
+	ActionStatus    Action = "status"
+)
+
+func (a Action) All() []Action {
+	return []Action{ActionInstall, ActionUninstall,
+		ActionStart, ActionStop,
+		ActionRestart, ActionStatus}
+}
+
 // Service is the interface of service
 type Service interface {
 	Start() (string, error)   // start the service
@@ -77,6 +129,7 @@ type Service interface {
 	Restart() (string, error) // restart the service gracefully
 	Status() (string, error)  // status of the service
 	Install() (string, error) // install the service
+	Uninstall() (string, error)
 }
 
 func emptyThenDefault(value, defaultValue string) string {
@@ -84,6 +137,15 @@ func emptyThenDefault(value, defaultValue string) string {
 		value = defaultValue
 	}
 	return value
+}
+
+func (o *serviceOption) getOSService() (service Service, err error) {
+	if o.Execer.OS() != fakeruntime.OSLinux && o.Execer.OS() != fakeruntime.OSDarwin {
+		err = fmt.Errorf("only support on Linux/Darwin instead of %s", o.Execer.OS())
+	} else {
+		service = newService(o.Execer, o.scriptPath)
+	}
+	return
 }
 
 func newService(execer fakeruntime.Execer, scriptPath string) (service Service) {
@@ -95,6 +157,8 @@ func newService(execer fakeruntime.Execer, scriptPath string) (service Service) 
 				scriptPath: emptyThenDefault(scriptPath, "/Library/LaunchDaemons/com.github.linuxsuren.atest.plist"),
 				script:     macOSServiceScript,
 			},
+			cli: "launchctl",
+			id:  "com.github.linuxsuren.atest",
 		}
 	case fakeruntime.OSLinux:
 		service = &linuxService{
@@ -108,6 +172,24 @@ func newService(execer fakeruntime.Execer, scriptPath string) (service Service) 
 	return
 }
 
+func (o *serviceOption) getContainerService() (service Service, err error) {
+	var client string
+	switch serviceMode(o.mode) {
+	case ServiceModeDocker:
+		client = "docker"
+	case ServiceModePodman, ServiceModeContainer:
+		client = "podman"
+	default:
+		err = fmt.Errorf("not support mode: '%s'", o.mode)
+		return
+	}
+
+	if client, err = o.LookPath(client); err == nil {
+		service = newContainerService(o.Execer, client, o.image, o.version)
+	}
+	return
+}
+
 type commonService struct {
 	fakeruntime.Execer
 	scriptPath string
@@ -116,6 +198,8 @@ type commonService struct {
 
 type macOSService struct {
 	commonService
+	cli string
+	id  string
 }
 
 var (
@@ -126,12 +210,12 @@ var (
 )
 
 func (s *macOSService) Start() (output string, err error) {
-	output, err = s.Execer.RunCommandAndReturn("sudo", "", "launchctl", "start", "com.github.linuxsuren.atest")
+	output, err = s.Execer.RunCommandAndReturn("sudo", "", s.cli, "start", s.id)
 	return
 }
 
 func (s *macOSService) Stop() (output string, err error) {
-	output, err = s.Execer.RunCommandAndReturn("sudo", "", "launchctl", "stop", "com.github.linuxsuren.atest")
+	output, err = s.Execer.RunCommandAndReturn("sudo", "", s.cli, "stop", s.id)
 	return
 }
 
@@ -143,14 +227,19 @@ func (s *macOSService) Restart() (output string, err error) {
 }
 
 func (s *macOSService) Status() (output string, err error) {
-	output, err = s.Execer.RunCommandAndReturn("sudo", "", "launchctl", "runstats", "system/com.github.linuxsuren.atest")
+	output, err = s.Execer.RunCommandAndReturn("sudo", "", s.cli, "runstats", s.id)
 	return
 }
 
 func (s *macOSService) Install() (output string, err error) {
 	if err = os.WriteFile(s.scriptPath, []byte(s.script), os.ModeAppend); err == nil {
-		output, err = s.Execer.RunCommandAndReturn("sudo", "", "launchctl", "enable", "system/com.github.linuxsuren.atest")
+		output, err = s.Execer.RunCommandAndReturn("sudo", "", s.cli, "enable", s.id)
 	}
+	return
+}
+
+func (s *macOSService) Uninstall() (output string, err error) {
+	output, err = s.Execer.RunCommandAndReturn("sudo", "", s.cli, "disable", s.id)
 	return
 }
 
@@ -182,5 +271,68 @@ func (s *linuxService) Install() (output string, err error) {
 	if err = os.WriteFile(s.scriptPath, []byte(s.script), os.ModeAppend); err == nil {
 		output, err = s.Execer.RunCommandAndReturn("systemctl", "", "enable", "atest")
 	}
+	return
+}
+
+func (s *linuxService) Uninstall() (output string, err error) {
+	output, err = s.Execer.RunCommandAndReturn("systemctl", "", "disable", "atest")
+	return
+}
+
+type containerService struct {
+	Execer fakeruntime.Execer
+	name   string
+	client string
+	image  string
+	tag    string
+}
+
+const defaultImage = "ghcr.io/linuxsuren/api-testing"
+
+func newContainerService(execer fakeruntime.Execer, client, image, tag string) (service Service) {
+	if tag == "" {
+		tag = "latest"
+	}
+	if image == "" {
+		image = defaultImage
+	}
+	service = &containerService{
+		Execer: execer,
+		client: client,
+		name:   "atest",
+		image:  image,
+		tag:    tag,
+	}
+	return
+}
+
+func (s *containerService) Start() (output string, err error) {
+	err = s.Execer.SystemCall(s.client, []string{s.client, "run", "--name=" + s.name,
+		"--restart=always", "-d", "--pull=always", "--network=host",
+		s.image + ":" + s.tag}, os.Environ())
+	return
+}
+
+func (s *containerService) Stop() (output string, err error) {
+	output, err = s.Execer.RunCommandAndReturn(s.client, "", "stop", s.name)
+	return
+}
+
+func (s *containerService) Restart() (output string, err error) {
+	output, err = s.Execer.RunCommandAndReturn(s.client, "", "restart", s.name)
+	return
+}
+
+func (s *containerService) Status() (output string, err error) {
+	return
+}
+
+func (s *containerService) Install() (output string, err error) {
+	output, err = s.Start()
+	return
+}
+
+func (s *containerService) Uninstall() (output string, err error) {
+	output, err = s.Stop()
 	return
 }
--- cmd/service_test.go
+++ cmd/service_test.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bytes"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/linuxsuren/api-testing/pkg/server"
@@ -23,6 +24,11 @@ func TestService(t *testing.T) {
 	err = notLinux.Execute()
 	assert.NotNil(t, err)
 
+	notSupportedMode := NewRootCmd(fakeruntime.FakeExecer{ExpectOS: "fake"}, NewFakeGRPCServer(), server.NewFakeHTTPServer())
+	notSupportedMode.SetArgs([]string{"service", paramAction, "install", "--mode=fake"})
+	notSupportedMode.SetOut(new(bytes.Buffer))
+	assert.NotNil(t, notSupportedMode.Execute())
+
 	tmpFile, err := os.CreateTemp(os.TempDir(), "service")
 	assert.Nil(t, err)
 	defer func() {
@@ -33,6 +39,7 @@ func TestService(t *testing.T) {
 		name         string
 		action       string
 		targetOS     string
+		mode         string
 		expectOutput string
 	}{{
 		name:         "action: start",
@@ -60,6 +67,11 @@ func TestService(t *testing.T) {
 		targetOS:     "linux",
 		expectOutput: "output4",
 	}, {
+		name:         "action: uninstall",
+		action:       "uninstall",
+		targetOS:     "linux",
+		expectOutput: "output4",
+	}, {
 		name:         "action: start, macos",
 		action:       "start",
 		targetOS:     fakeruntime.OSDarwin,
@@ -84,17 +96,69 @@ func TestService(t *testing.T) {
 		action:       "install",
 		targetOS:     fakeruntime.OSDarwin,
 		expectOutput: "output4",
+	}, {
+		name:         "action: uninstall, macos",
+		action:       "uninstall",
+		targetOS:     fakeruntime.OSDarwin,
+		expectOutput: "output4",
+	}, {
+		name:         "start in podman",
+		action:       "start",
+		targetOS:     fakeruntime.OSLinux,
+		mode:         string(ServiceModePodman),
+		expectOutput: "",
+	}, {
+		name:         "start in docker",
+		action:       "start",
+		targetOS:     fakeruntime.OSLinux,
+		mode:         string(ServiceModeDocker),
+		expectOutput: "",
+	}, {
+		name:         "stop in docker",
+		action:       "stop",
+		targetOS:     fakeruntime.OSLinux,
+		mode:         string(ServiceModeDocker),
+		expectOutput: "",
+	}, {
+		name:         "restart in docker",
+		action:       "restart",
+		targetOS:     fakeruntime.OSLinux,
+		mode:         string(ServiceModeDocker),
+		expectOutput: "",
+	}, {
+		name:         "status in docker",
+		action:       "status",
+		targetOS:     fakeruntime.OSLinux,
+		mode:         string(ServiceModeDocker),
+		expectOutput: "",
+	}, {
+		name:         "install in docker",
+		action:       "install",
+		targetOS:     fakeruntime.OSLinux,
+		mode:         string(ServiceModeDocker),
+		expectOutput: "",
+	}, {
+		name:         "uninstall in docker",
+		action:       "uninstall",
+		targetOS:     fakeruntime.OSLinux,
+		mode:         string(ServiceModeDocker),
+		expectOutput: "",
 	}}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			if tt.mode == "" {
+				tt.mode = string(ServiceModeOS)
+			}
+
 			buf := new(bytes.Buffer)
 			normalRoot := NewRootCmd(fakeruntime.FakeExecer{ExpectOS: tt.targetOS, ExpectOutput: tt.expectOutput},
 				NewFakeGRPCServer(), server.NewFakeHTTPServer())
 			normalRoot.SetOut(buf)
-			normalRoot.SetArgs([]string{"service", "--action", tt.action, "--script-path", tmpFile.Name()})
+			normalRoot.SetArgs([]string{"service", "--action", tt.action,
+				"--script-path", tmpFile.Name(), "--mode", tt.mode, "--image="})
 			err = normalRoot.Execute()
 			assert.Nil(t, err)
-			assert.Equal(t, tt.expectOutput+"\n", buf.String())
+			assert.Equal(t, tt.expectOutput, strings.TrimSpace(buf.String()))
 		})
 	}
 }
