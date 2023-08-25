19f3134508c56f72a2cdb94c21e98488643bd42d
rick <1450685+LinuxSuRen@users.noreply.github.com>
2023-08-25 16:15:16 +0800

    ui: add a welcome page when no any test suites

---
+++ console/atest-ui/src/views/WelcomePage.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>Welcome to use atest to improve your code quality!</div>
+    <div>Please read the following guide if this is your first time to use atest.</div>
+    <li>Create a test suite on the left panel</li>
+    <li>Select a suite, then create the test case.</li>
+
+    <div>
+        Please get more details from the <a href="https://linuxsuren.github.io/api-testing/" target="_blank" rel="noopener">offical document</a>.
+    </div>
+</template>
