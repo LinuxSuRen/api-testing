3f2877e6c292ca2e9e26e85b2d9ba6fb20293b54
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2023-09-02 15:18:07 +0800

    fix: the operator cluster role permission is missing (#198)

--- operator/controllers/atest_controller.go
+++ operator/controllers/atest_controller.go
@@ -49,6 +49,12 @@ type ATestReconciler struct {
 //+kubebuilder:rbac:groups=core.linuxsuren.github.com,resources=atests/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=core.linuxsuren.github.com,resources=atests/finalizers,verbs=update
 
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=persistentvolumeclaims,verbs=get;list;watch;create;update;patch;delete
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 // TODO(user): Modify the Reconcile function to compare the state specified by
