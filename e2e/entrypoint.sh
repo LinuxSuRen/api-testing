83c5ebe0fc473305c509980214e1aa78ee659207
rick <LinuxSuRen@users.noreply.github.com>
2023-11-30 09:29:45 +0000

    test: add unit test cases for github reporting

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -2,14 +2,17 @@
 set -e
 
 mkdir -p /root/.config/atest
+mkdir -p /var/data
 
 nohup atest server&
+cmd="atest run -p test-suite-common.yaml --report github --report-github-identity e2e-testing --report-file /var/data/report.json --report-github-repo linuxsuren/api-testing --report-github-pr ${PULL_REQUEST:-0}"
 
-echo "start to run testing"
-kind=orm target=mysql:3306 driver=mysql atest run -p test-suite-common.yaml
-kind=orm target=mariadb:3306 driver=mysql atest run -p test-suite-common.yaml
-kind=etcd target=etcd:2379 atest run -p test-suite-common.yaml
-kind=mongodb target=mongo:27017 atest run -p test-suite-common.yaml
+echo "start to run testing: $cmd"
+kind=orm target=mysql:3306 driver=mysql $cmd
+kind=orm target=mariadb:3306 driver=mysql $cmd
+kind=etcd target=etcd:2379 $cmd
+kind=mongodb target=mongo:27017 $cmd
+kind=orm target=postgres:5432 driver=postgres $cmd
 
 # TODO online git repository is unstable, need to fix
 # if [ -z "$GITEE_TOKEN" ]
@@ -21,7 +24,6 @@ kind=mongodb target=mongo:27017 atest run -p test-suite-common.yaml
 # fi
 
 # TODO need to fix below cases
-kind=orm target=postgres:5432 driver=postgres atest run -p test-suite-common.yaml
 # kind=s3 target=minio:9000 atest run -p test-suite-common.yaml
 
 cat /root/.config/atest/stores.yaml
