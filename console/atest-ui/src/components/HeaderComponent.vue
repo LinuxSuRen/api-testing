fded85bd00cd33d31ee1570a78ae123ce7293484
yuluo-yx <yuluo08290126@gmail.com>
2024-05-07 16:50:00 +0800

    featL optimize
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- console/atest-ui/src/components/HeaderComponent.vue
+++ console/atest-ui/src/components/HeaderComponent.vue
@@ -33,8 +33,8 @@
 </template>
 
 <script lang="ts" setup>
-import { ref, inject } from 'vue'
-import { Sunny, Moon } from '@element-plus/icons-vue'
+import { ref } from 'vue'
+// import { Sunny, Moon } from '@element-plus/icons-vue'
 import { setAsDarkTheme } from '../theme/theme'
 
 const darkOpt = ref(false)
