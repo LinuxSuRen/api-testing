b9a7fa85608074ecf5a22dd241d2c2eafdfe8ab0
rick <linuxsuren@users.noreply.github.com>
2024-05-14 22:13:53 +0800

    complete the basic start and stop server feat

---
+++ console/atest-ui/my-electron-app/api.js
@@ -0,0 +1,15 @@
+exports.control = function(okCallback, errorCallback) {
+    fetch('http://localhost:' + getPort() + '/healthz').
+        then(okCallback).catch(errorCallback)
+}
+
+function getPort() {
+    return 7788
+}
+
+function getHomePage() {
+    return 'http://localhost:' + getPort()
+}
+
+exports.getPort = getPort
+exports.getHomePage = getHomePage
