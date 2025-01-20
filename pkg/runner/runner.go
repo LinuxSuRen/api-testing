c1356e5948578cfa832b98424c7896edf350a449
rick <LinuxSuRen@users.noreply.github.com>
2025-01-20 18:35:21 +0800

    fix the unit tests

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -17,58 +17,58 @@ limitations under the License.
 package runner
 
 import (
-	"context"
-	"fmt"
-	"io"
-	"strings"
+    "context"
+    "fmt"
+    "io"
+    "strings"
 
-	"github.com/linuxsuren/api-testing/pkg/testing"
-	fakeruntime "github.com/linuxsuren/go-fake-runtime"
+    "github.com/linuxsuren/api-testing/pkg/testing"
+    fakeruntime "github.com/linuxsuren/go-fake-runtime"
 )
 
 // TestCaseRunner represents a test case runner
 type TestCaseRunner interface {
-	RunTestCase(testcase *testing.TestCase, dataContext interface{}, ctx context.Context) (output interface{}, err error)
-	GetSuggestedAPIs(suite *testing.TestSuite, api string) ([]*testing.TestCase, error)
-	WithSecure(secure *testing.Secure)
-	WithOutputWriter(io.Writer)
-	WithWriteLevel(level string)
-	WithTestReporter(TestReporter)
-	WithExecer(fakeruntime.Execer)
-	WithSuite(*testing.TestSuite)
-	WithAPISuggestLimit(int)
+    RunTestCase(testcase *testing.TestCase, dataContext interface{}, ctx context.Context) (output interface{}, err error)
+    GetSuggestedAPIs(suite *testing.TestSuite, api string) ([]*testing.TestCase, error)
+    WithSecure(secure *testing.Secure)
+    WithOutputWriter(io.Writer)
+    WithWriteLevel(level string)
+    WithTestReporter(TestReporter)
+    WithExecer(fakeruntime.Execer)
+    WithSuite(*testing.TestSuite)
+    WithAPISuggestLimit(int)
 }
 
 // HTTPResponseRecord represents a http response record
 type ResponseRecord interface {
-	GetResponseRecord() SimpleResponse
+    GetResponseRecord() SimpleResponse
 }
 
 // SimpleResponse represents a simple response
 type SimpleResponse struct {
-	Header     map[string]string
-	Body       string
-	RawBody    []byte
-	StatusCode int
+    Header     map[string]string
+    Body       string
+    RawBody    []byte
+    StatusCode int
 }
 
 func (s SimpleResponse) getFileName() string {
-	for k, v := range s.Header {
-		if k == "Content-Disposition" {
-			return strings.TrimSuffix(strings.TrimPrefix(v, `attachment; filename="`), `"`)
-		}
-	}
-	return ""
+    for k, v := range s.Header {
+        if k == "Content-Disposition" {
+            return strings.TrimSuffix(strings.TrimPrefix(v, `attachment; filename="`), `"`)
+        }
+    }
+    return ""
 }
 
 // NewDefaultUnimplementedRunner initializes an unimplementedRunner using the default values.
 func NewDefaultUnimplementedRunner() UnimplementedRunner {
-	return UnimplementedRunner{
-		testReporter: NewDiscardTestReporter(),
-		writer:       io.Discard,
-		log:          NewDefaultLevelWriter("info", io.Discard),
-		execer:       fakeruntime.NewDefaultExecer(),
-	}
+    return UnimplementedRunner{
+        testReporter: NewDiscardTestReporter(),
+        writer:       io.Discard,
+        log:          NewDefaultLevelWriter("info", io.Discard),
+        execer:       fakeruntime.NewDefaultExecer(),
+    }
 }
 
 // UnimplementedRunner implements interface TestCaseRunner except method RunTestCase.
@@ -77,54 +77,56 @@ func NewDefaultUnimplementedRunner() UnimplementedRunner {
 // It is recommended to use NewDefaultUnimplementedRunner to initalize rather than
 // to fill it manully.
 type UnimplementedRunner struct {
-	testReporter TestReporter
-	writer       io.Writer
-	log          LevelWriter
-	execer       fakeruntime.Execer
-	Secure       *testing.Secure
-	proxy        *testing.Proxy
+    testReporter TestReporter
+    writer       io.Writer
+    log          LevelWriter
+    execer       fakeruntime.Execer
+    Secure       *testing.Secure
+    proxy        *testing.Proxy
 }
 
 func (r *UnimplementedRunner) RunTestCase(testcase *testing.TestCase, dataContext interface{}, ctx context.Context) (output interface{}, err error) {
-	return nil, fmt.Errorf("unimplemented")
+    return nil, fmt.Errorf("unimplemented")
 }
 
 // WithOutputWriter sets the io.Writer
 func (r *UnimplementedRunner) WithOutputWriter(writer io.Writer) {
-	r.writer = writer
+    r.writer = writer
 }
 
 // WithWriteLevel sets the level writer
 func (r *UnimplementedRunner) WithWriteLevel(level string) {
-	if level != "" {
-		r.log = NewDefaultLevelWriter(level, r.writer)
-	}
+    if level != "" {
+        r.log = NewDefaultLevelWriter(level, r.writer)
+    }
 }
 
 // WithTestReporter sets the TestReporter
 func (r *UnimplementedRunner) WithTestReporter(reporter TestReporter) {
-	r.testReporter = reporter
+    r.testReporter = reporter
 }
 
 // WithExecer sets the execer
 func (r *UnimplementedRunner) WithExecer(execer fakeruntime.Execer) {
-	r.execer = execer
+    r.execer = execer
 }
 
 // WithSecure sets the secure option.
 func (r *UnimplementedRunner) WithSecure(secure *testing.Secure) {
-	r.Secure = secure
+    r.Secure = secure
 }
 
 func (r *UnimplementedRunner) GetSuggestedAPIs(suite *testing.TestSuite, api string) (result []*testing.TestCase, err error) {
-	// empty implement
-	return
+    // empty implement
+    return
 }
 
 func (r *UnimplementedRunner) WithAPISuggestLimit(int) {
-	// empty implement
+    // empty implement
 }
 
 func (s *UnimplementedRunner) WithSuite(suite *testing.TestSuite) {
-	s.proxy = suite.Proxy
+    if suite != nil {
+        s.Secure = suite.Spec.Secure
+    }
 }
