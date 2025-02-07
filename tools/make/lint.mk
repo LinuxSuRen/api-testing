31eb9d4d4a53f087ac11757c693d6477c6d0df67
yuluo-yx <yuluo08290126@gmail.com>
2025-02-07 22:47:26 +0800

    fix: fix  makefile tools install
    Signed-off-by: yuluo-yx <yuluo08290126@gmail.com>

--- tools/make/lint.mk
+++ tools/make/lint.mk
@@ -29,14 +29,14 @@ lint.yamllint: $(tools/yamllint)
 .PHONY: lint.markdown
 lint: lint.markdown
 lint-deps: $(tools/markdownlint)
-lint.markdown:
+lint.markdown: $(tools/markdownlint)
 	@$(LOG_TARGET)
 	$(tools/markdownlint) -c tools/linter/markdownlint/markdown_lint_config.json docs/site/content/**
 
 .PHONY: lint.markdown.fix
 lint: lint.markdown.fix
 lint-deps: $(tools/markdownlint)
-lint.markdown.fix:
+lint.markdown.fix: $(tools/markdownlint)
 	@$(LOG_TARGET)
 	$(tools/markdownlint) -c tools/linter/markdownlint/markdown_lint_config.json --fix docs/site/content/**
 
@@ -44,20 +44,20 @@ lint.markdown.fix:
 lint: lint.codespell
 lint-deps: $(tools/codespell)
 lint.codespell: CODESPELL_SKIP := $(shell cat tools/linter/codespell/.codespell.skip | tr \\n ',')
-lint.codespell:
+lint.codespell: $(tools/codespell)
 	@$(LOG_TARGET)
 	$(tools/codespell) --skip $(CODESPELL_SKIP) --ignore-words tools/linter/codespell/.codespell.ignorewords --check-filenames
 
 .PHONY: lint.checklinks
 lint: lint.checklinks
 lint-deps: $(tools/linkinator)
-lint.checklinks: # Check for broken links in the docs
+lint.checklinks: $(tools/linkinator) # Check for broken links in the docs
 	@$(LOG_TARGET)
 	$(tools/linkinator) docs/site/public/ -r --concurrency 25 --skip $(LINKINATOR_IGNORE)
 
 .PHONY: lint.checklicense
 lint: lint.checklicense
 lint-deps: $(tools/skywalking-eyes)
-lint.checklicense: # Check for broken links in the docs
+lint.checklicense: $(tools/skywalking-eyes) # Check for broken links in the docs
 	@$(LOG_TARGET)
 	$(tools/skywalking-eyes) -c tools/linter/license/.licenserc.yaml header check
