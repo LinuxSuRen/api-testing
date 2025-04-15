967ec53324fc3d6142963cb9268b95db634a9fac
Rick <1450685+LinuxSuRen@users.noreply.github.com>
2025-04-15 15:23:28 +0800

    feat: support to start extension via http (#664)
    * feat: support to start extension via http
    
    * fix the extension address checking
    
    ---------
    
    Co-authored-by: rick <LinuxSuRen@users.noreply.github.com>

--- cmd/server.go
+++ cmd/server.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2023-2024 API Testing Authors.
+Copyright 2023-2025 API Testing Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -424,7 +424,7 @@ func startPlugins(storeExtMgr server.ExtManager, kinds *server.StoreKinds) (err 
 	const socketPrefix = "unix://"
 
 	for _, kind := range kinds.Data {
-		if kind.Enabled && strings.HasPrefix(kind.Url, socketPrefix) {
+		if kind.Enabled && (strings.HasPrefix(kind.Url, socketPrefix) || strings.Contains(kind.Url, ":")) {
 			err = errors.Join(err, storeExtMgr.Start(kind.Name, kind.Url))
 		}
 	}
