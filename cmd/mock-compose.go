ebe03a0073cd84e88dd791eba9155419d2d97fa4
rick <linuxsuren@users.noreply.github.com>
2025-04-22 22:36:27 +0800

    add comments

--- cmd/mock-compose.go
+++ cmd/mock-compose.go
@@ -1,51 +1,68 @@
+/*
+Copyright 2025 API Testing Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package cmd
 
 import (
-    "github.com/linuxsuren/api-testing/pkg/mock"
-    "github.com/spf13/cobra"
-    "os"
-    "os/signal"
-    "syscall"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/linuxsuren/api-testing/pkg/mock"
+	"github.com/spf13/cobra"
 )
 
 func createMockComposeCmd() (c *cobra.Command) {
-    c = &cobra.Command{
-        Use:   "mock-compose",
-        Short: "Mock a server",
-        Args:  cobra.ExactArgs(1),
-        RunE: func(cmd *cobra.Command, args []string) (err error) {
-            reader := mock.NewLocalFileReader(args[0])
+	c = &cobra.Command{
+		Use:   "mock-compose",
+		Short: "Mock multiple servers",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			reader := mock.NewLocalFileReader(args[0])
 
-            var server *mock.Server
-            if server, err = reader.Parse(); err != nil {
-                return
-            }
+			var server *mock.Server
+			if server, err = reader.Parse(); err != nil {
+				return
+			}
 
-            var subServers []mock.DynamicServer
-            for _, proxy := range server.Proxies {
-                subProxy := &mock.Server{
-                    Proxies: []mock.Proxy{proxy},
-                }
+			var subServers []mock.DynamicServer
+			for _, proxy := range server.Proxies {
+				subProxy := &mock.Server{
+					Proxies: []mock.Proxy{proxy},
+				}
 
-                subReader := mock.NewObjectReader(subProxy)
-                subServer := mock.NewInMemoryServer(c.Context(), proxy.Port)
-                if err = subServer.Start(subReader, proxy.Prefix); err != nil {
-                    return
-                }
-                subServers = append(subServers, subServer)
-            }
+				subReader := mock.NewObjectReader(subProxy)
+				subServer := mock.NewInMemoryServer(c.Context(), proxy.Port)
+				if err = subServer.Start(subReader, proxy.Prefix); err != nil {
+					return
+				}
+				subServers = append(subServers, subServer)
+			}
 
-            clean := make(chan os.Signal, 1)
-            signal.Notify(clean, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
-            select {
-            case <-c.Context().Done():
-            case <-clean:
-            }
-            for _, server := range subServers {
-                server.Stop()
-            }
-            return
-        },
-    }
-    return
+			clean := make(chan os.Signal, 1)
+			signal.Notify(clean, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
+			select {
+			case <-c.Context().Done():
+			case <-clean:
+			}
+			for _, server := range subServers {
+				server.Stop()
+			}
+			return
+		},
+	}
+	return
 }
